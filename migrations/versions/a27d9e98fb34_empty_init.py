62b991ba777d0f921bdbba017fbfdd14c582069b
mmwandera <mmwandera74@gmail.com>
2024-02-13 18:01:43 +0300

    completed assignment

---
+++ migrations/versions/a27d9e98fb34_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a27d9e98fb34
+Revises: 
+Create Date: 2024-02-13 17:56:44.865324
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a27d9e98fb34'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
